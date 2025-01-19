d0b9ea74f2d2ac97321c723bb62849b47843422b
WhitWander <vcmikhaelcp@gmail.com>
2025-01-19 11:10:47 -0300

    header

--- ticket/src/App.js
+++ ticket/src/App.js
@@ -1,8 +1,10 @@
-import Patterns from "./Components/Patterns.js/index.js";
+import Header from "./Components/Header/index.js";
+import Patterns from "./Components/Patterns/index.js";
 
 function App() {
   return (
     <div className="main">
+      <Header />
       <Patterns />
     </div>
   );
---
+++ ticket/src/Components/Header/index.js
@@ -0,0 +1,11 @@
+import './Header.css'
+
+export default function Header() {
+    return (
+        <div className="container">
+            <img className="logo" src="/images/logo-full.svg" alt="" />
+            <h1 className="title">Your Journey to Coding Conf <br />2025 Starts Here!</h1>
+            <p className="text">Secure your spot at next year's biggest coding conference.</p>
+        </div>
+    )
+}
